64fede1996104cc17fc055ff39c3f5497ad39570
Quintin Herb <33743358+quintanamo@users.noreply.github.com>
2019-04-28 21:09:39 -0400

    Add files via upload

---
+++ EntreLink/src/edu/ycp/cs320/entrelink/servlet/NewPostServlet.java
@@ -0,0 +1,37 @@
+package edu.ycp.cs320.entrelink.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class NewPostServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		HttpSession session=req.getSession();
+		session.getAttribute("loggedInName");
+		
+		System.out.println("Search Servlet: doGet");
+		
+		req.getRequestDispatcher("/_view/new_post.jsp").forward(req, resp);
+	}	
+	protected void doOpenProjects(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/projects.jsp").forward(req, resp);
+	}
+	protected void doOpenProfile(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/profile.jsp").forward(req, resp);
+	}
+	protected void doOpenSearch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/search.jsp").forward(req, resp);
+	}
+	protected void doOpenHome(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/index.jsp").forward(req, resp);
+	}
+}
---
+++ EntreLink/src/edu/ycp/cs320/entrelink/servlet/SignupServlet.java
@@ -0,0 +1,37 @@
+package edu.ycp.cs320.entrelink.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SignupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		HttpSession session=req.getSession();
+		session.getAttribute("loggedInName");
+		Boolean sessionExists = session.isNew();
+        
+		
+		System.out.println("Index Servlet: doGet");
+		
+		req.getRequestDispatcher("/_view/signup.jsp").forward(req, resp);
+	}
+	
+	protected void doOpenProjects(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/projects.jsp").forward(req, resp);
+	}
+	protected void doOpenProfile(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/profile.jsp").forward(req, resp);
+	}
+	protected void doOpenHome(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/index.jsp").forward(req, resp);
+	}
+}

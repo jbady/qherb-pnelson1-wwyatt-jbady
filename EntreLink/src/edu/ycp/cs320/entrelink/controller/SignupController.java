d83294f5b66d6fe12ba6e2069c331c4fa2f5275b
Quintin Herb <33743358+quintanamo@users.noreply.github.com>
2019-05-01 09:37:03 -0400

    Add files via upload

---
+++ EntreLink/src/edu/ycp/cs320/entrelink/controller/SignupController.java
@@ -0,0 +1,53 @@
+package edu.ycp.cs320.entrelink.controller;
+
+import java.util.List;
+
+import edu.ycp.cs320.entrelink.model.NewUser;
+import edu.ycp.cs320.entrelink.model.User;
+import edu.ycp.cs320.entrelink.userdb.persist.DatabaseProvider;
+import edu.ycp.cs320.entrelink.userdb.persist.DerbyDatabase;
+import edu.ycp.cs320.entrelink.userdb.persist.FakeDatabase;
+import edu.ycp.cs320.entrelink.userdb.persist.IDatabase;
+
+public class SignupController {
+	
+	private NewUser model;
+	
+	public void setModel() {
+		this.model = model;
+	}
+	
+	
+	IDatabase db = new DerbyDatabase();
+	User getUser;
+	
+	public boolean verifyIsNewUser() {
+		if(model != null) {
+			if(db.findUserByEmailOrUsername(model.getNewUsername()).equals(null)) {
+				if(db.findUserByEmailOrUsername(model.getNewEmail()).equals(null)) {
+					return true;
+				}
+			}
+		}
+	return false;
+	}
+	
+	public boolean verifyEmailsAreSame() {
+		if(model.getNewEmail().equals(model.getConfirmEmail())) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean verifyPasswordsAreSame() {
+		if(model.getNewPassword().equals(model.getConfirmPassword())) {
+			return true;
+		}
+		return false;
+	}
+	
+	public NewUser getModel() {
+		return model;
+	}
+	
+}

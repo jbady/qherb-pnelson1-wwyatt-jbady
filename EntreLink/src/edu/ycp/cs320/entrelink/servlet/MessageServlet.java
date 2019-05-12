e40a5f073df5cf2bad03128cab18af944951965a
Patrick Nelson <48131480+pdnelson@users.noreply.github.com>
2019-05-12 15:17:52 -0400

    Add files via upload

---
+++ EntreLink/src/edu/ycp/cs320/entrelink/servlet/MessageServlet.java
@@ -0,0 +1,58 @@
+package edu.ycp.cs320.entrelink.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import edu.ycp.cs320.entrelink.controller.AllPostsController;
+import edu.ycp.cs320.entrelink.controller.MessageController;
+import edu.ycp.cs320.entrelink.model.Message;
+import edu.ycp.cs320.entrelink.model.Post;
+
+public class MessageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private MessageController controller = null;	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		HttpSession session=req.getSession();
+		if(session.getAttribute("loggedInName") != null) {
+			int userId = (int) session.getAttribute("loggedInId");
+			
+			System.out.println("Message Servlet: doGet");	
+			
+			// Loading all the messages for the logged in user...
+			ArrayList<Message> messages = null;
+			String errorMessage = null;
+			controller = new MessageController();
+			
+			// get list of messages returned from query
+			messages = controller.getAllMessagesForLoggedInUser(userId);
+			
+			// Add result objects as request attributes
+			req.setAttribute("errorMessage", errorMessage);
+			req.setAttribute("messages", messages);
+		}
+		req.getRequestDispatcher("/_view/messages.jsp").forward(req, resp);
+	}	
+	protected void doOpenProjects(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/projects.jsp").forward(req, resp);
+	}
+	protected void doOpenProfile(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/profile.jsp").forward(req, resp);
+	}
+	protected void doOpenSearch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/search.jsp").forward(req, resp);
+	}
+	protected void doOpenHome(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/index.jsp").forward(req, resp);
+	}
+}
---
+++ EntreLink/src/edu/ycp/cs320/entrelink/servlet/NewMessageServlet.java
@@ -0,0 +1,45 @@
+package edu.ycp.cs320.entrelink.servlet;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import edu.ycp.cs320.entrelink.controller.PostController;
+import edu.ycp.cs320.entrelink.model.DateModifier;
+
+public class NewMessageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	//private PostController controller = null;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		HttpSession session=req.getSession();
+		session.getAttribute("loggedInName");
+		
+		System.out.println("New Message Servlet: doGet");
+		
+		req.getRequestDispatcher("/_view/newMessage.jsp").forward(req, resp);
+	}	
+	protected void doOpenProjects(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/projects.jsp").forward(req, resp);
+	}
+	protected void doOpenProfile(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/profile.jsp").forward(req, resp);
+	}
+	protected void doOpenSearch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/search.jsp").forward(req, resp);
+	}
+	protected void doOpenHome(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException  {
+		req.getRequestDispatcher("/_view/index.jsp").forward(req, resp);
+	}
+}
